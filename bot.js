c0d727163316f5d34bc25ab42a5973594565a21d
Ariyaman Debnath <ariyamandebnath.ad@gmail.com>
2024-01-06 00:28:18 +0530

    script for making the telegram bot work

--- bot.js
+++ bot.js
@@ -1,37 +1,30 @@
-const Telegram = require('node-telegram-bot-api')
+const Telegram = require('node-telegram-bot-api');
+const { Configuration, OpenAIApi } = require("openai");
 
-const {Configuration, OpenAIApi}= require("openai")
+const botToken = "write the telegram bot_token";
+const open_AI_TOKEN = "Your api key";
 
-const botToken ="write the telegram bot_token"
+const config = new OpenAIApi.Configuration({
+    apiKey: open_AI_TOKEN,
+});
 
-const open_AI_TOKEN = "Your api key"
+const openAI = new OpenAIApi(config);
 
-const config= new Configuration({
-    apikey: open_AI_TOKEN,
+const bot = new Telegram(botToken, { polling: true });
 
-})
+bot.onText(/\/start/, (msg) => {
+    bot.sendMessage(msg.chat.id, "Welcome to Arin the Chat_bot.\nAsk me anything!!");
+});
 
-const openAI = new OpenAIApi(config)
+bot.on("message", async (msg) => {
+    const chatId = msg.chat.id;
 
-const bot = new Telegram(botToken,{ polling:true})
+    const reply = await openAI.createCompletion({
+        max_tokens: 100,
+        model: "ada", // use model as per your choice of affordability
+        prompt: msg.text,
+        temperature: 0.5,
+    });
 
-bot.onText(/\/start/, (msg)=>[
-    bot.sendMessage(msg.chat.id, "welocme to Arin the Chat_bot.\n ask me anything!!")
-])
-
-bot.on("message",async (msg)=>{
-  const chatId =msg.chat.id
-
-  const reply = await openAI.createCompletion({
-
-    max_tokens:100,
-    model: "ada",//use model as per your choice of affordibility
-    prompt: msg.text,
-    temperature: 0.5,
-
-  })
-
-
-
-  bot.sendMessage(chatId, reply.data.choices[0].text)
-})
+    bot.sendMessage(chatId, reply.data.choices[0].text);
+});
