40b51372a068d5f7a5ad17266c25bfcae2b1470e
Ariyaman Debnath <ariyamandebnath.ad@gmail.com>
2024-01-06 00:26:01 +0530

    script for making the telegram bot work

--- bot.js
+++ bot.js
@@ -1,5 +1,37 @@
 const Telegram = require('node-telegram-bot-api')
 
-const botToken =
+const {Configuration, OpenAIApi}= require("openai")
 
-const bot = new Telegram()
+const botToken ="write the telegram bot_token"
+
+const open_AI_TOKEN = "Your api key"
+
+const config= new Configuration({
+    apikey: open_AI_TOKEN,
+
+})
+
+const openAI = new OpenAIApi(config)
+
+const bot = new Telegram(botToken,{ polling:true})
+
+bot.onText(/\/start/, (msg)=>[
+    bot.sendMessage(msg.chat.id, "welocme to Arin the Chat_bot.\n ask me anything!!")
+])
+
+bot.on("message",async (msg)=>{
+  const chatId =msg.chat.id
+
+  const reply = await openAI.createCompletion({
+
+    max_tokens:100,
+    model: "ada",//use model as per your choice of affordibility
+    prompt: msg.text,
+    temperature: 0.5,
+
+  })
+
+
+
+  bot.sendMessage(chatId, reply.data.choices[0].text)
+})
